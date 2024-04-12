552fd71a970e32ceeff88640ad173076095881ba
tjennr <j.truong@yale.edu>
2024-04-12 16:59:17 -0400

    stash

--- cell.py
+++ cell.py
@@ -3,7 +3,7 @@ class Cell:
         self._x = x
         self._y = y
         self._height = 0
-        self._occupied = False
+        self._occupied_by = None
 
     def build(self):
         self._height += 1
@@ -15,7 +15,16 @@ class Cell:
         return f'{self._x}, {self._y}'
     
     def is_occupied(self):
-        return self._occupied
+        if self._occupied_by is None:
+            return False
+        else:
+            return True
     
-    def in_bounds(self, x, y):
-        pass
+    def occupy(self, worker):
+        self._occupied_by = worker
+
+    def get_occupying_worker(self):
+        return self._occupied_by
+
+    def remove(self):
+        self._occupied_by = None
