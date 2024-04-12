0204b64235c71ec9f9c4d6daa19fe2b3c04925da
tjennr <j.truong@yale.edu>
2024-04-12 16:56:48 -0400

    stash

--- cli.py
+++ cli.py
@@ -6,22 +6,28 @@ class SantoriniCLI:
 
     def __init__(self):
         self._board = Board()
-        self._playerWhite = PlayerWhite()
-        self._playerBlue = PlayerBlue()
-        self._turn_count = 0
+        self._playerWhite = PlayerWhite(self._board)
+        self._playerBlue = PlayerBlue(self._board)
+        self._turn_count = 1
 
     def _display_board(self):
         board = self._board.get_cells()
         for row in board:
             print("+--+--+--+--+--+")
-            print("|" + "|".join(f"{cell.get_height()} " for cell in row) + "|")
+            row_string = ""
+            for cell in row:
+                if cell.is_occupied():
+                    row_string += f"|{cell.get_height()}{cell.get_occupying_worker()}"
+                else:
+                    row_string += f"|{cell.get_height()} "
+            print(row_string + "|")
         print("+--+--+--+--+--+")
 
     def run(self):
         while True:
             self._display_board()
 
-            if self._turn_count % 2 == 0:
+            if self._turn_count % 2 == 1:
                 player = self._playerWhite
             else:
                 player = self._playerBlue
@@ -35,23 +41,33 @@ class SantoriniCLI:
                 worker = input("Select a worker to move\n")
 
             # Select move direction
-            new_pos = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
-            while new_pos not in ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']:
-                print("Not a valid direction")
-                new_pos = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
-            player.move(worker, new_pos)
+            while True:
+                try:
+                    dir = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
+                    if dir not in ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']:
+                        print("Not a valid direction")
+                        continue
+                    player.move(worker, dir)
+                    break
+                except:
+                    print(f"Cannot move {dir}")
 
             # Select build direction
-            build_pos = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
-            while new_pos not in ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']:
-                print("Not a valid direction")
-                new_pos = input("Select a direction to move (n, ne, e, se, s, sw, w, nw)\n")
-            player.build(build_pos)
+            while True:
+                try:
+                    dir = input("Select a direction to build (n, ne, e, se, s, sw, w, nw)\n")
+                    if dir not in ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw']:
+                        print("Not a valid direction")
+                        continue
+                    player.build(worker, dir)
+                    break
+                except:
+                    print(f"Cannot build {dir}")
             
             self._increment_turn_count()
 
     def _increment_turn_count(self):
-        self._increment_turn_count += 1
+        self._turn_count += 1
 
 
 if __name__ == "__main__":
