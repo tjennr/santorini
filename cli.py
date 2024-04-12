5bf623fb38a713e34a8a6c7d020befbb5865a7d8
Brandon Nguyen <bxngyn@Brandons-Air-8.wireless.yale.internal>
2024-04-12 12:03:38 -0400

    board

--- cli.py
+++ cli.py
@@ -2,9 +2,20 @@ class SantoriniCLI:
     def __init__(self):
         pass
     def _display_board(self):
-        pass
+        print(f"""+--+--+--+--+--+
+|0 |0 |0 |0 |0 |
++--+--+--+--+--+
+|0 |0Y|0 |0B|0 |
++--+--+--+--+--+
+|0 |0 |0 |0 |0 |
++--+--+--+--+--+
+|0 |0A|0 |0Z|0 |
++--+--+--+--+--+
+|0 |0 |0 |0 |0 |
++--+--+--+--+--+""")
+
     def run(self):
-        pass
+        self._display_board()
 
 if __name__ == "__main__":
-    SantoriniCLI.run()
+    SantoriniCLI().run()
