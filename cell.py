5a9e1b91ad9d6104427ed5ce2c7038fc83505ec7
tjennr <j.truong@yale.edu>
2024-04-15 12:18:08 -0400

    Added docstrings to cell.py and created unt2.txt"

--- cell.py
+++ cell.py
@@ -7,25 +7,32 @@ class Cell:
         self._occupied_by = None
 
     def build(self):
+        '''Increments the height of the cell's building'''
         self._height += 1
 
     def get_height(self):
+        '''Returns the height of the cell's building'''
         return self._height
     
     def get_position(self):
+        '''Returns the (x, y) coordinate position of the cell'''
         return f'{self._x}, {self._y}'
     
     def is_occupied(self):
+        '''Returns True if the cell has a worker occupying it'''
         if self._occupied_by is None:
             return False
         else:
             return True
     
-    def occupy(self, worker):
-        self._occupied_by = worker
+    def occupy(self, worker_name):
+        '''Sets a worker as occupying the cell'''
+        self._occupied_by = worker_name
 
     def get_occupying_worker(self):
+        '''Returns the name of the worker occuping the cell'''
         return self._occupied_by
 
     def remove(self):
+        '''Removes the worker current occupying the cell'''
         self._occupied_by = None
