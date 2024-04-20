817e3dfb6a8fb1d5af503575175e253970bfeaab
tjennr <j.truong@yale.edu>
2024-04-20 12:49:18 -0400

    Command

--- command.py
+++ command.py
@@ -2,26 +2,21 @@ from abc import ABC, abstractmethod
 
 class Command(ABC):
     '''Abstract command interface'''
+    def __init__(self, manager, worker, direction):
+        self._manager = manager
+        self._worker = worker
+        self._direction = direction
+
     @abstractmethod
     def execute(self):
         pass
 
 class MoveCommand(Command):
     '''Concrete command for move'''
-    def __init__(self, manager, worker, direction):
-        self._manager = manager
-        self._worker = worker
-        self._direction = direction
-    
     def execute(self):
         self._manager.move(self._worker, self._direction)
 
 class BuildCommand(Command):
     '''Concrete command for build'''
-    def __init__(self, manager, worker, direction):
-        self._manager = manager
-        self._worker = worker
-        self._direction = direction
-    
     def execute(self):
         self._manager.build(self._worker, self._direction)
